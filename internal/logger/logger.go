e86c2aa4c37240cbf14977e02e84b6dde25e07ad
m-mdy-m <bitsgenix@gmail.com>
2026-02-01 19:11:31 +0330

    feat(logger): add logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,48 +3,125 @@ package logger
 import (
 	"fmt"
 	"os"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/fatih/color"
 )
 
-func Info(m string) {
-	fmt.Println(m)
+type Level int
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+)
+
+var (
+	currentLevel Level = LevelInfo
+	mu           sync.Mutex
+	timeFormat   = "15:04:05"
+)
+
+func SetLevel(l Level) {
+	mu.Lock()
+	defer mu.Unlock()
+	currentLevel = l
 }
 
-func Infof(format string, args ...any) {
-	Info(fmt.Sprintf(format, args...))
+func ParseLevel(s string) Level {
+	switch strings.ToLower(s) {
+	case "debug":
+		return LevelDebug
+	case "info":
+		return LevelInfo
+	case "warn", "warning":
+		return LevelWarn
+	case "error":
+		return LevelError
+	case "fatal":
+		return LevelFatal
+	default:
+		return LevelInfo
+	}
 }
 
-func Warning(m string) {
-	color.Yellow("⚠ %s", m)
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return "DEBUG"
+	case LevelInfo:
+		return "INFO"
+	case LevelWarn:
+		return "WARN"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	default:
+		return "UNKNOWN"
+	}
 }
 
-func Warningf(format string, args ...any) {
-	Warning(fmt.Sprintf(format, args...))
+func isEnabled(l Level) bool {
+	mu.Lock()
+	defer mu.Unlock()
+	return l >= currentLevel
 }
 
-func Error(m string) {
-	color.New(color.FgRed).Fprintln(os.Stderr, "✗", m)
+func timestamp() string {
+	return time.Now().Format(timeFormat)
 }
 
-func Errorf(format string, args ...any) error {
-	err := fmt.Errorf(format, args...)
-	Error(err.Error())
-	return err
+func Debug(tag, msg string) {
+	if !isEnabled(LevelDebug) {
+		return
+	}
+	color.New(color.FgCyan).Fprintf(os.Stderr, "[%s] [DEBUG] [%s] %s\n", timestamp(), tag, msg)
 }
-func Verbose(m string) {
-	color.Cyan("→ %s", m)
+func Debugf(tag, format string, args ...any) {
+	Debug(tag, fmt.Sprintf(format, args...))
+}
+func Info(tag, msg string) {
+	if !isEnabled(LevelInfo) {
+		return
+	}
+	color.New(color.FgWhite).Fprintf(os.Stdout, "[%s] [INFO ] [%s] %s\n", timestamp(), tag, msg)
+}
+func Infof(tag, format string, args ...any) {
+	Info(tag, fmt.Sprintf(format, args...))
 }
 
-func Verbosef(format string, args ...any) {
-	Verbose(fmt.Sprintf(format, args...))
+func Warn(tag, msg string) {
+	if !isEnabled(LevelWarn) {
+		return
+	}
+	color.New(color.FgYellow).Fprintf(os.Stderr, "[%s] [WARN ] [%s] %s\n", timestamp(), tag, msg)
 }
 
-func Fatal(m string) {
-	Error(m)
+func Warnf(tag, format string, args ...any) {
+	Warn(tag, fmt.Sprintf(format, args...))
+}
+
+func Error(tag, msg string) {
+	if !isEnabled(LevelError) {
+		return
+	}
+	color.New(color.FgRed).Fprintf(os.Stderr, "[%s] [ERROR] [%s] %s\n", timestamp(), tag, msg)
+}
+
+func Errorf(tag, format string, args ...any) {
+	Error(tag, fmt.Sprintf(format, args...))
+}
+
+func Fatal(tag, msg string) {
+	color.New(color.FgRed, color.Bold).Fprintf(os.Stderr, "[%s] [FATAL] [%s] %s\n", timestamp(), tag, msg)
 	os.Exit(1)
 }
 
-func Fatalf(format string, args ...any) {
-	Fatal(fmt.Sprintf(format, args...))
+func Fatalf(tag, format string, args ...any) {
+	Fatal(tag, fmt.Sprintf(format, args...))
 }
