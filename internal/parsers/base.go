fd115ef25021340d8112149e4cfff07880a8412b
m-mdy-m <bitsgenix@gmail.com>
2026-02-01 19:38:55 +0330

    fix(parser): fix extract

--- internal/parsers/base.go
+++ internal/parsers/base.go
@@ -30,34 +30,12 @@ func GetParser(proto common.Kind) Parser {
 }
 
 var (
-	vlessPattern = regexp.MustCompile(`vless://[^\s\r\n]+`)
-	vmessPattern = regexp.MustCompile(`vmess://[^\s\r\n]+`)
-	ssPattern    = regexp.MustCompile(`ss://[^\s\r\n]+`)
+	vlessPattern = regexp.MustCompile(`\bvless://[^\s\r\n]+`)
+	vmessPattern = regexp.MustCompile(`\bvmess://[^\s\r\n]+`)
+	ssPattern    = regexp.MustCompile(`\bss://[^\s\r\n]+`)
 )
 
 var allPatterns = []*regexp.Regexp{vlessPattern, vmessPattern, ssPattern}
-
-func ExtractURIs(text string) []string {
-	logger.Debugf(tag, "extracting URIs from text (%d chars)", len(text))
-
-	seen := map[string]bool{}
-	var results []string
-
-	for _, pattern := range allPatterns {
-		matches := pattern.FindAllString(text, -1)
-		for _, m := range matches {
-			m = cleanURI(m)
-			if seen[m] {
-				continue
-			}
-			seen[m] = true
-			results = append(results, m)
-		}
-	}
-
-	logger.Infof(tag, "extracted %d unique URI(s) from text", len(results))
-	return results
-}
 
 func cleanURI(uri string) string {
 	trimChars := ".,;:!?)}\"]'»›"
