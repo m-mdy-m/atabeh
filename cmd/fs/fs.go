00349fd7808c813fc5e50a348b8515b9c9849016
m-mdy-m <bitsgenix@gmail.com>
2026-02-06 04:51:02 +0330

    refactor: update fs package

--- cmd/fs/fs.go
+++ cmd/fs/fs.go
@@ -1,7 +1,6 @@
 package fs
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -14,34 +13,22 @@ func BaseDir() string {
 	return filepath.Join(home, ".config", "atabeh")
 }
 
-func EnsureBaseDir() error {
-	dir := BaseDir()
-	return os.MkdirAll(dir, 0o755)
-}
-
 func DBPath() string {
 	return filepath.Join(BaseDir(), "atabeh.db")
 }
-func EnsureDBDir() error {
-	dir := filepath.Dir(DBPath())
-	return os.MkdirAll(dir, 0o755)
-}
-func EnsureFileDir(filePath string) error {
-	dir := filepath.Dir(filePath)
-	return os.MkdirAll(dir, 0o755)
-}
+
 func EnsureDirs() error {
 	dirs := []string{
 		BaseDir(),
 		filepath.Join(BaseDir(), "cache"),
 		filepath.Join(BaseDir(), "logs"),
-		filepath.Join(BaseDir(), "subscriptions"),
 	}
 
-	for _, d := range dirs {
-		if err := os.MkdirAll(d, 0o755); err != nil {
-			return fmt.Errorf("cannot create dir %q: %w", d, err)
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return err
 		}
 	}
+
 	return nil
 }
